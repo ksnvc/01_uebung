b6b56f6f7f40e919472609930e247b9a5a8774a2
Sara Kosanovic <sarakosanovic@Saras-MacBook-Pro.local>
2019-10-08 13:36:19 +0200

    loesung

--- src/main/java/LRUCache.java
+++ src/main/java/LRUCache.java
@@ -1,13 +1,53 @@
+import java.util.LinkedList;
 
 public class LRUCache implements Cache {
 
     private int size;
     private Storage storage;
     private int hits, misses;
+    private LinkedList<Integer> cache = new LinkedList();
 
     public LRUCache(int size) {
         this.storage = new Storage();
 
     }
 
+    @Override
+    public int get(int e) {
+        int index = cache.indexOf(e);
+        if(index >=0){
+            int el = cache.get(index);
+            cache.remove(el);
+            cache.addFirst(el);
+            if(size>4){
+            cache.removeLast();}
+            hits += 1;
+            return(el);
+        }
+        int el = storage.get(e);
+        cache.addFirst(el);
+        cache.removeLast();
+        misses +=1;
+        return(el);
+
+    }
+
+    @Override
+    public int getHits() {
+        return hits;
+    }
+
+    @Override
+    public int getMisses() {
+        return misses;
+    }
+
+    @Override
+    public Integer[] getCacheOrder() {
+        Integer[]array = cache.toArray(new Integer[cache.size()]);
+        for (int i = 0; i < cache.size(); i++) {
+            array[i] = cache.get(i);
+        }
+      return array;
+    }
 }
--- src/main/java/RandomCache.java
+++ src/main/java/RandomCache.java
@@ -5,15 +5,45 @@ public class RandomCache implements Cache {
 
     private Storage storage;
     private int size;
-    private int hits;
-    private int misses;
+    private int hits=0;
+    private int misses=0;
     private Random rnd = new Random();
-
+    private Integer[] cache;
 
 
     public RandomCache(int size) {
         this.storage = new Storage();
-
+        this.cache = new java.lang.Integer[size];
     }
 
+    @Override
+    public int get(int e) {
+        for(int i : cache){
+        if(cache[i]==null){
+            continue;}
+           if(cache[i]== e){
+               hits+=1;
+               return(cache[i]);
+           }
+        }
+        int random = rnd.nextInt(size);
+        cache[random-1] = storage.get(e);
+            misses+=1;
+        return(cache[random]);
+    }
+
+    @Override
+    public int getHits() {
+        return hits;
+    }
+
+    @Override
+    public int getMisses() {
+        return misses;
+    }
+
+    @Override
+    public Integer[] getCacheOrder() {
+        return cache;
+    }
 }
